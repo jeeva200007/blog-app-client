1f41d48b7ae7f10b1f04fc2fc791e81d56df373c
Jeeva A <120329868+jeeva200007@users.noreply.github.com>
2024-06-17 13:59:56 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Posts from "../components/Posts";
+import { useNavigate } from "react-router";
+import { UserContext } from "../context/userContext";
 
 const Home = () => {
+  const { currentUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const token = currentUser?.token;
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div>
       <Posts />
